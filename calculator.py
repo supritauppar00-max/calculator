21bb73fbf0115ea3badbb70a76b7030d46d3ebab
supritauppar00-max <supritauppar00@gmail.com>
2026-09-18 03:05:03 -0700

    Implement addition and subtraction in calculator.py

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# Addition and subtraction of two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+addition = a + b
+subtraction = a - b
+
+print("Addition =", addition)
+print("Subtraction =", subtraction)
